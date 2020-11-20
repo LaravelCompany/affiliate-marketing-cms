178401c16697e31417daf2a1aa15eb75aa0f0cc8
Stefan <stefan@lzomedia.com>
2020-11-20 10:43:50 +0000

    Added more changes to the code

--- app/Http/Controllers/SeoToolsController.php
+++ app/Http/Controllers/SeoToolsController.php
@@ -1,37 +1,51 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\SeoTools;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Session;
+use Illuminate\View\View;
 
+/**
+ * Class SeoToolsController
+ * @package App\Http\Controllers
+ */
 class SeoToolsController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
+    /**
+     * @method index
+     * @return Factory|Application|View
+     */
     public function index()
     {
+
         $tools = SeoTools::find(1);
-        return view('admin.seotools',compact('tools'));
+
+        return view('admin.seo',compact('tools'));
     }
 
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
+     * @method update
+     * @param Request $request
      * @param int $id
      * @param SeoTools $seoTools
-     * @return \Illuminate\Http\Response
+     * @return Application|RedirectResponse|Response|Redirector
      */
     public function update(Request $request, $id, SeoTools $seoTools)
     {
